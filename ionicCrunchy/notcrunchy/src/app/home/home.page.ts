a4428cd920ff62dab79a88794928788113f82fe9
Wladimir Acevedo <wladimir.acevedo.237@gmail.com>
2023-02-17 22:07:40 -0300

    Se agrego nueva pag y loader para api

--- ionicCrunchy/notcrunchy/src/app/home/home.page.ts
+++ ionicCrunchy/notcrunchy/src/app/home/home.page.ts
@@ -1,53 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimeService } from '../service/anime.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
-
 export class HomePage implements OnInit {
-
   slideOpts={
     slidesPerView: 2,
     spaceBetween: 10,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true
+    }
+  }
+
+  idAnime: string = '';
+  recent_anime: any[] = [];
+  popular_anime: any[] = [];
+
+  constructor( 
+    private anime: AnimeService,
+    private loadingController: LoadingController
+  ) {}
+
+  ngOnInit(): void {
+    this.anime.getAnime('recent-release').subscribe(data => {
+      this.recent_anime = Object.values(data);
+      console.log(data);
+    });
+
+    this.anime.getAnime('popular').subscribe(data => {
+      this.popular_anime = Object.values(data);
+    });
+  }
+  
+  
+
+  async presentLoading(show: boolean) {
+    console.log("presentLoading called with show=", show);
+    const loading = await this.loadingController.create({
+      message: 'Cargando...'
+    });
+    
+
+    if (show) {
+      await loading.present();
+    } if(loading !== undefined) {
+      await loading.dismiss();
+    }
+    
+  }
+
+  async infoAnime(id: string) {
+    this.presentLoading(true);
+    
+
+    this.anime.getAnime(`search?keyw=${id}`).subscribe(
+      data => {
+        console.log(data);
+        
+        this.presentLoading(false);
+       
+      },
+      error => {
+        console.log(error);
+       
+        
+      }
+    );
   }
 }
-
-recent_anime:any=[]
-  constructor( 
-    private anime: AnimeService
-    ) {}
-
-ngOnInit(): void {
-
-  this.anime.getAnime('recent-release').subscribe(data=>{
-
-    this.recent_anime=Object.values(data)
-
-    console.log(data)
-
-  });
-
-
-}
-
-
- 
-// Using the example ID of "call-of-the-night-2234".
-
-}
-
-
- 
-
-
-
